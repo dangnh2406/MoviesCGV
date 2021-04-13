e5887639288a6bdadf924f20e94a87c738714c23
dangnh2406 <dangnh2406@gmail.com>
2021-04-13 21:43:45 +0700

    create repo

---
+++ app/src/main/java/com/nguyenhaidang_dangnh2406/moviescgv/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nguyenhaidang_dangnh2406.moviescgv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
